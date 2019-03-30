3d895865f76e75941c833aa7a51aa76faa2cfc7a
listenerri <listenerri@gmail.com>
2019-03-30 20:53:34 +0800

    fix error in python 34

--- python/blog-github-webhooks.py
+++ python/blog-github-webhooks.py
@@ -3,11 +3,10 @@
 
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
-from http.server import HTTPStatus
 
 class MyBaseHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
+        self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(b'Only handle POST requests.\n')
@@ -17,9 +16,9 @@ class MyBaseHTTPRequestHandler(BaseHTTPRequestHandler):
 
 def main():
     port = 2345
-    with HTTPServer(("", port), MyBaseHTTPRequestHandler) as httpd:
-        print("Running on port: %d"%(port))
-        httpd.serve_forever()
+    httpd = HTTPServer(("", port), MyBaseHTTPRequestHandler)
+    print("Running on port: %d"%(port))
+    httpd.serve_forever()
 
 if __name__ == "__main__":
     main()
